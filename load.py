a0fa768b4d0a35d4065538b210300ce50e1946f4
Ryan Choi <ryan.choi@gmail.com>
2015-01-12 23:26:20 -0800

    now getting through 17 records

--- load.py
+++ load.py
@@ -6,60 +6,39 @@ from bigquery import schema_from_record
 from utils import *
 from config import *
 
-# import logging
-# import sys
-# 
-# root = logging.getLogger()
-# root.setLevel(logging.DEBUG)
-# 
-# ch = logging.StreamHandler(sys.stdout)
-# ch.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# ch.setFormatter(formatter)
-# root.addHandler(ch)
-
-# record_str = read_file(SAMPLE_TWEET_FILE)
-# record = json.loads(record_str)
-# schema_str = schema_from_record(record)
-# print schema_str
-# exit()
-
-schema_str = read_file(SCHEMA_FILE)
-schema = json.loads(schema_str)
-# print "schema: %s" % schema
-
-# get client
-client = get_client(PROJECT_ID, service_account=SERVICE_ACCOUNT, private_key=KEY, readonly=False)
-client.swallow_results = False
-print "client: %s" % client
-
-# create table
-created = client.create_table(DATASET_ID, TABLE_ID, schema)
-print "created: %s" % created
-
-record = json.loads(read_file(SAMPLE_TWEET_FILE))
-print "record: %s" % record
+import logging
+import sys
  
-inserted = client.push_rows(DATASET_ID, TABLE_ID, [record], None) 
-print "inserted: %s" % inserted
+def enable_logging():
 
-row = 0
- 
-with open(SAMPLE_STREAM_FILE, "r") as f:
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+     
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
+def generate_schema_from_tweet():
     
-    for tweet in f:
-         
-        record = json.loads(tweet)
-        record = scrub(record)
-        
-        inserted = client.push_rows(DATASET_ID, TABLE_ID, [record], None) 
-         
-        if inserted.get('insertErrors', None):
+    record_str = read_file(SAMPLE_TWEET_FILE)
+    record = json.loads(record_str)
+    schema_str = schema_from_record(record)
+    print schema_str
+    exit()
 
-            print "record %s: %s" % (row, record)
-            print "error: %s" % inserted
-            
-            print "missing: "
+def insert_record(client, record, schema_str=None):
+     
+    result = client.push_rows(DATASET_ID, TABLE_ID, [record], None) 
+     
+    if result.get('insertErrors', None):
+
+        print "Record: %s" % (json.dumps(record))
+        print "Error result: %s" % result
+        
+        if schema_str:
+            print "Missing: "
             ak = all_keys(record)
       
             for k in ak:
@@ -69,9 +48,52 @@ with open(SAMPLE_STREAM_FILE, "r") as f:
                     print "%s: %s" % (key, has)
                     break
 
-            break
-         
-        row = row + 1
+        return False
+    
+    return True
+
+def main():
+    
+    enable_logging()
+    
+    # get client
+    client = get_client(PROJECT_ID, service_account=SERVICE_ACCOUNT, private_key=KEY, readonly=False)
+    client.swallow_results = False
+    print "client: %s" % client
+    
+    schema_str = read_file(SCHEMA_FILE)
+    schema = json.loads(schema_str)
+    
+    # un-comment to create table
+    created = client.create_table(DATASET_ID, TABLE_ID, schema)
+    print "created: %s" % created
+    
+    # un-comment to load a single file
+#     record = json.loads(read_file(SAMPLE_TWEET_FILE))
+#     success = insert_record(client, record, schema_str)
+#     print "Single record: %s" % success
+#     if not success:
+#         return
+    
+    row = 0
+    with open(SAMPLE_STREAM_FILE, "r") as f:
+        
+        for tweet in f:
+            
+            print "Process row: %s" % row
+             
+            record = json.loads(tweet)
+            record_scrubbed = scrub(record)
+            
+            success = insert_record(client, record_scrubbed, schema_str)
+            if not success:
+                print "Record: %s" % (json.dumps(record))
+                return
+            
+            row = row + 1
+
+if __name__ == "__main__":
+    main()
 
 # # Submit an async query.
 # job_id, _results = client.query('SELECT * FROM dataset.my_table LIMIT 1000')
