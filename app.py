0980215b44b36fa20f6d230d8bec50279d7d6d5f
Ryan Choi <ryan.choi@gmail.com>
2015-01-13 22:25:17 -0800

    formatting

--- app.py
+++ app.py
@@ -26,13 +26,13 @@ class ShowChartPage(webapp2.RequestHandler):
     
     @decorator.oauth_required
     def get(self):
-	temp_data = {}
-	temp_path = 'Templates/chart.html'
-	queryData = {'query':'SELECT word FROM [publicdata:samples.shakespeare] LIMIT 1000'}
-	tableData = get_service().jobs()
-	response = tableData.query(projectId=PROJECT_NUMBER, body=queryData).execute()
-	self.response.out.write(response)
-	# self.response.out.write(template.render(temp_path,temp_data))
+    	temp_data = {}
+    	temp_path = 'Templates/chart.html'
+    	queryData = {'query':'SELECT word FROM [publicdata:samples.shakespeare] LIMIT 1000'}
+    	tableData = get_service().jobs()
+    	response = tableData.query(projectId=PROJECT_NUMBER, body=queryData).execute()
+    	self.response.out.write(response)
+    	# self.response.out.write(template.render(temp_path,temp_data))
 	
 class ShowHome(webapp2.RequestHandler):
     
@@ -44,54 +44,51 @@ class ShowHome(webapp2.RequestHandler):
 
 class GetChartData(webapp2.RequestHandler):
     
-  @decorator.oauth_required
-  def get(self):
-    inputData = self.request.get("inputData")
-    queryData = {'query':'SELECT SUM(word_count) as WCount,corpus_date,group_concat(corpus) as Work FROM '
-'[publicdata:samples.shakespeare] WHERE word="' + inputData + '" and corpus_date>0 GROUP BY corpus_date ORDER BY WCount'}
-    tableData = get_service().jobs()
-    dataList = tableData.query(projectId=PROJECT_NUMBER, body=queryData).execute()
+    @decorator.oauth_required
+    def get(self):
+        inputData = self.request.get("inputData")
+        queryData = {'query':'SELECT SUM(word_count) as WCount,corpus_date,group_concat(corpus) as Work FROM '
+                     '[publicdata:samples.shakespeare] WHERE word="' + inputData + '" and corpus_date>0 GROUP BY corpus_date ORDER BY WCount'}
+        tableData = get_service().jobs()
+        dataList = tableData.query(projectId=PROJECT_NUMBER, body=queryData).execute()
     
-    resp = []
-    if 'rows' in dataList:
-      for row in dataList['rows']:
-        for key, dict_list in row.iteritems():
-          count = dict_list[0]
-          year = dict_list[1]
-          corpus = dict_list[2]
-          resp.append({'count': count['v'], 'year':year['v'], 'corpus':corpus['v']})
-    else:
-      resp.append({'count':'0', 'year':'0', 'corpus':'0'})
+        resp = []
+        if 'rows' in dataList:
+            for row in dataList['rows']:
+                for key, dict_list in row.iteritems():
+                    count = dict_list[0]
+                    year = dict_list[1]
+                    corpus = dict_list[2]
+                    resp.append({'count': count['v'], 'year':year['v'], 'corpus':corpus['v']})
+        else:
+            resp.append({'count':'0', 'year':'0', 'corpus':'0'})
     
- 	
-    self.response.headers['Content-Type'] = 'application/json'
-    self.response.out.write(json.dumps(resp))
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(resp))
 
 class DisplayChart(webapp2.RequestHandler):
     
-  @decorator.oauth_required
-  def get(self):
-    template_data = {}
-    template_path = 'Templates/displayChart.html'
-    self.response.out.write(template.render(template_path, template_data))
+    @decorator.oauth_required
+    def get(self):
+        template_data = {}
+        template_path = 'Templates/displayChart.html'
+        self.response.out.write(template.render(template_path, template_data))
 
 class DisplayChart3(webapp2.RequestHandler):
     
-  @decorator.oauth_required
-  def get(self):
-    template_data = {}
-    template_path = 'Templates/displayChart_3.html'
-    self.response.out.write(template.render(template_path, template_data))
-    
+    @decorator.oauth_required
+    def get(self):
+        template_data = {}
+        template_path = 'Templates/displayChart_3.html'
+        self.response.out.write(template.render(template_path, template_data))
 
 class DisplayChart4(webapp2.RequestHandler):
     
-  @decorator.oauth_required
-  def get(self):
-    template_data = {}
-    template_path = 'Templates/displayChart_4.html'
-    self.response.out.write(template.render(template_path, template_data))
-     
+    @decorator.oauth_required
+    def get(self):
+        template_data = {}
+        template_path = 'Templates/displayChart_4.html'
+        self.response.out.write(template.render(template_path, template_data))
  
 application = webapp2.WSGIApplication([
     ('/chart', ShowChartPage),
