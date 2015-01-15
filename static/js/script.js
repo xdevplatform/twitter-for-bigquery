bf7ec0107d40095634718b9f5131156e74ee7e3e
Ryan Choi <ryan.choi@gmail.com>
2015-01-15 15:43:33 -0800

    charts

---
+++ static/js/script.js
@@ -0,0 +1,118 @@
+$(document).ready(function(){
+
+	$("#source").change(function(){
+    	var val = $(this).val();
+    	if (val == "hashtags"){
+    		$("#hashtags_holder").fadeIn();
+    	} else {
+    		$("#hashtags_holder").hide();
+    	}
+    	queryData();
+    });
+	
+	$("#pivot").change(function(){
+    	queryData();
+    });
+	
+	$("#charttype").change(function(){
+    	queryData();
+    });
+    
+    $("#source").change();
+	
+})
+
+function queryData() {
+
+	var source = $("#source").val();
+	var pivot = $("#pivot").val();
+
+	// chart type: http://c3js.org/examples.html
+	var chart = $("#chart").val();
+
+	var args = null;
+
+	if (chart == 'donut') {
+
+		args = {
+			bindto : '#chart',
+			data : {
+				columns : [ [ 'data1', 30 ], [ 'data2', 120 ], ],
+				type : 'donut',
+				onclick : function(d, i) {
+					console.log("onclick", d, i);
+				},
+				onmouseover : function(d, i) {
+					console.log("onmouseover", d, i);
+				},
+				onmouseout : function(d, i) {
+					console.log("onmouseout", d, i);
+				}
+			},
+			donut : {
+				title : "Iris Petal Width"
+			}
+		}
+
+	} else if (chart == 'timeseries') {
+
+		args = {
+			bindto : '#chart',
+			data : {
+				x : 'x',
+				// xFormat: '%Y%m%d', // 'xFormat' can be used as custom format
+				// of 'x'
+				columns : [
+						[ 'x', '2013-01-01', '2013-01-02', '2013-01-03',
+								'2013-01-04', '2013-01-05', '2013-01-06' ],
+						// ['x', '20130101', '20130102', '20130103', '20130104',
+						// '20130105', '20130106'],
+						[ 'data1', 30, 200, 100, 400, 150, 250 ],
+						[ 'data2', 130, 340, 200, 500, 250, 350 ] ]
+			},
+			axis : {
+				x : {
+					type : 'timeseries',
+					tick : {
+						format : '%Y-%m-%d'
+					}
+				}
+			}
+		}
+
+	} else if (chart == 'map') {
+
+	}
+
+	makeChart(args);
+
+	/*
+	 * $.ajax({ type: "GET", url: "/data", data: { inputData: "" }, dataType:
+	 * "json", success: function(response) { console.log(response);
+	 * makeChart(response); }, error: function(xhr, errorType, exception) {
+	 * console.log('Error occured'); } });
+	 */
+
+}
+
+function makeChart(args){
+	
+	c3.generate({
+	    bindto: '#chart',
+	    data: {
+	      columns: [
+	        ['data1', 30, 200, 100, 400, 150, 250],
+	        ['data2', 50, 20, 10, 40, 15, 25]
+	      ],
+	      axes: {
+	        data2: 'y2' // ADD
+	      }
+	    },
+	    axis: {
+	      y2: {
+	        show: true // ADD
+	      }
+	    }
+	});
+
+}
