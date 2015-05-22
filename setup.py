77e4bc428634b31726a1267cb4c2992f6175f29c
Ryan Choi <ryan.choi@gmail.com>
2015-05-21 21:39:39 -0700

    cleanup twitter path

--- setup.py
+++ setup.py
@@ -15,18 +15,13 @@ elif (config.MODE == "twitter"):
 else:
 	print "Invalid Mode. Mode can be 'gnip' or 'twitter' only";
 
-# files for app
+# files for app and image all in root
 shutil.copy2(schema, "./schema.json");
-
-# files for image
-shutil.copy2("./config", "./image/config");
-shutil.copy2("key.p12", "./image/key.p12");
-shutil.copy2(schema, "./image/schema.json");
 
 container_template = Utils.read_file("./container.yaml.template")
 container_template = Template(container_template).render(config)
 
-container_file = open("./image/container.yaml", 'w')
+container_file = open("./container.yaml", 'w')
 container_file.write(container_template)
 
 
