61a67d19fe087f43c06d32260b67cb081cd075c8
Ryan Choi <ryan.choi@gmail.com>
2015-05-26 00:18:52 -0700

    re-zip file after processing

--- batch.py
+++ batch.py
@@ -11,35 +11,28 @@ def process_file(file_gz, output=None):
 
 	file = file_gz[:-3]
 	
+	# unpack file
 	call_unzip = "gunzip %s" % (file_gz)
 	print call_unzip
 	os.system(call_unzip)
 	
+	# load to bigquery
 	call_batch = "bq load --source_format=NEWLINE_DELIMITED_JSON --max_bad_records=5000 %s %s" % (table, file)
 	print call_batch
 	os.system(call_batch)
+
+	# pack file
+	call_zip = "gzip %s" % (file)
+	print call_zip
+	os.system(call_zip)
 	
 	if output:
-		output.put("%s: Done" % (file))
+		output.put(file_gz)
 
-def info(title):
-    print(title)
-    print('module name:', __name__)
-    if hasattr(os, 'getppid'):  # only available on Unix
-        print('parent process:', os.getppid())
-    print('process id:', os.getpid())
-
-def f(name):
-    info('function f')
-    print('hello', name)
+	return file_gz
 
 if __name__ == '__main__':
 	
-# 	info('main line')
-# 	p = Process(target=f, args=('bob',))
-# 	p.start()
-# 	p.join()
-#     
  	files = []
  	processes = []
  	
